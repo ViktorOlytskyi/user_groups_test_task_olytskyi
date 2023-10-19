8c1157fa2e66c9d119fa0e8c0eb3e60d9803c821
viktor <veglasmamona@gmail.com>
2023-10-19 17:15:50 +0300

    v1.3

--- user_groups_project/groups/tests.py
+++ user_groups_project/groups/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Group
 
-# Create your tests here.
+
+class GroupTests(TestCase):
+    serialized_rollback = True
+    def setUp(self):
+        self.group = Group.objects.create(name='Test Group', description='Test Description')
+
+    def test_add_group_page(self):
+        response = self.client.get(reverse('add_group'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Add new group')
+
+    def test_add_group(self):
+        group = Group.objects.create(name='Test Group')
+        group_count_before = Group.objects.count()
+        data = {'name': 'Test Group', 'description': 'Test Description'}
+        response = self.client.post(reverse('add_group'), data)
+        group_count_after = Group.objects.count()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(group_count_after, group_count_before + 1)
+
+        new_group = Group.objects.last()
+        self.assertEqual(new_group.name, 'Test Group')
+        self.assertEqual(new_group.description, 'Test Description')
+        return group
+    def test_edit_group_page(self):
+        response = self.client.get(reverse('edit_group', args=[self.group.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Edit group')
+
+    def test_edit_group(self):
+        data = {'name': 'Updated Group', 'description': 'Updated Description'}
+        response = self.client.post(reverse('edit_group', args=[self.group.id]), data)
+        updated_group = Group.objects.get(id=self.group.id)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(updated_group.name, 'Updated Group')
+        self.assertEqual(updated_group.description, 'Updated Description')
--- user_groups_project/users/tests.py
+++ user_groups_project/users/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import User
+from ..groups.tests import GroupTests
 
-# Create your tests here.
+
+class UserTests(TestCase):
+
+    def test_add_user_page(self):
+        response = self.client.get(reverse('add_user'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Add new user')
+
+    def test_add_user(self):
+        user_count_before = User.objects.count()
+        data = {'name': 'Test User', 'group': 1}
+        response = self.client.post(reverse('add_user'), data)
+        user_count_after = User.objects.count()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(user_count_after, user_count_before + 1)
+
+    def setUp(self):
+        group = GroupTests().test_add_group()
+        self.user = User.objects.create(name='Test User', group=group)
+
+    def test_edit_user_page(self):
+        response = self.client.get(reverse('edit_user', args=[self.user.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test User')
+
+    def test_edit_user(self):
+        data = {'name': 'Test User', 'group': 1}
+        response = self.client.post(reverse('edit_user', args=[self.user.id]), data)
+        updated_user = User.objects.get(id=self.user.id)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(updated_user.name, 'Test User')
+        self.assertEqual(updated_user.group_id, 1)
